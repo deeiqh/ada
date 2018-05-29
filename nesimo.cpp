e13d6c4fe59de71d6ef3f7c08f249c1bc81019e6
Kevin <kast46.15@gmail.com>
2018-05-29 16:17:03 -0500

    Update nesimo.cpp

--- nesimo.cpp
+++ nesimo.cpp
@@ -3,32 +3,40 @@
 using namespace std;
 
 int particiona(int A[], int p, int r);
-int nesimo(int B[], int tam, int pos, int p, int r);
+int nesimo(int B[], int pos, int p, int r);
 
 int main()
 {
 	int n = 10;
 	int A[n] = {99,33,55,77,11,22,88,66,33,44};			
-    cout << A[particiona(A,0,n-1)] << '\n';
+    //cout << A[particiona(A,0,n-1)] << '\n';
     
     n = 13;
-    int B[n] = {15,10,12,4,6,13,1,5,2,9,10,7,8};    
-    cout << nesimo(B,n,2,0,n-1) << '\n';
+    int B[n] = {15,10,12,4,6,13,1,5,2,9,10,7,8};   
+                // 1,2,4,5,6,7,8,9,10,10,12,13,15
+    cout << nesimo(B,12,0,n-1) << '\n';
     //APLICAR A UVA 10041 Y VER LA COMPLEJIDAD DE NESIMO
     
 	return 0;
 }
 
-int nesimo(int B[], int tam, int pos, int p, int r)
+int nesimo(int B[], int pos, int p, int r)
 {
-    int pos_p = particiona(B,p,r);cout << "pos_p = " <<pos_p << "\n";
+    int pos_p = particiona(B,p,r);
+    /*
+    cout << "pos_p=" <<pos_p << " p=" <<p<< " r="<<r<<  "\n";
+    for (int i = 0; i<= 12; i++)
+        cout << B[i] << " ";
+    cout << '\n' << '\n';
+    */
+    
     if( pos_p == pos){
         return B[pos_p];
     }
     if(pos < pos_p)
-        nesimo(B,pos_p, pos,0,pos_p-1);
+        nesimo(B,pos,0,pos_p-1);
     else
-        nesimo(B,tam-pos_p-1, pos, pos_p+1, tam-1);
+        nesimo(B,pos, pos_p+1, r);
 }
 
 int particiona(int A[], int p, int r)
@@ -49,7 +57,6 @@ int particiona(int A[], int p, int r)
     A[r] = t;
     return i+1;
 }
-
 
 
 
